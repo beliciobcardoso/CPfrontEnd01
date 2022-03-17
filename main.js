b049db2a3ca721a84de9f876e869e43738ab6df5
unknown <ezioweb@hotmail.com>
2022-03-16 22:43:12 -0300

    ataulizando JS

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ let tituloPostReferencia = document.querySelector('#tituloPost')
 let textoPostReferencia = document.querySelector('#textoPost')
 let imgPostReferencia = document.querySelector('#imgPost')
 
+botaoPostarReferencia.addEventListener('click', function(event){
+    event.preventDefault()
+
+    let post = {
+        titulo: tituloPostReferencia,
+        texto: textoPostReferencia,
+        imagem: imgPostReferencia
+    }
+})
